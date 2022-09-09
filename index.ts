4c2acb9bb7873774477faf28e89b9b68f126126c
Stefano Nepa <nepa@bryter.io>
2022-09-09 13:00:12 +0300

    session 09.09.2022

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import { createMachine, interpret, assign } from "xstate";
-import "./styles.css";
+import { createMachine, interpret, assign } from 'xstate';
+import { log } from 'xstate/lib/actions';
+import './styles.css';
 
-document.getElementById("app").innerHTML = `
+document.getElementById('app').innerHTML = `
 <h1>XState TypeScript Example</h1>
 <div>
   Open the <strong>Console</strong> to view the machine output.
@@ -9,71 +10,106 @@ document.getElementById("app").innerHTML = `
 </div>
 `;
 
-
 interface Context {
-    retries: number;
+  retries: number;
 }
 
-const btn = document.getElementById("retry-btn");
-const fetchMachine = createMachine<Context>({
-    id: "fetch",
-    initial: "idle",
-    context: {
-        retries: 0
-    },
-    states: {
-        idle: {
-            on: {
-                FETCH: "loading"
-            }
-        },
-        loading: {
-            invoke: {
-                src: () => {
-                    return Promise.reject();
-                    // return fetch("https://fakeapi.andreaspabst.com/api/todos")
-                    //   .then((response) => response.json())
-                    //   .then((json) => console.log(json));
-                },
-                onDone: "success",
-                onError: "failure"
-            }
-        },
-        success: {
-            type: "final",
-            entry: () => {
-                console.log("success entered");
-            }
-        },
-        failure: {
-            on: {
-                RETRY: {
-                    target: "loading",
-                    actions: assign({
-                        retries: (context, event) => context.retries + 1
-                    })
-                }
-            },
-            entry: () => {
-                console.log("failure entered");
-            }
-        }
-    }
-});
+const btn = document.getElementById('retry-btn');
 
-const service = interpret(fetchMachine).onTransition((state) => {
-    console.log(`CURRENT STATE: ${state.value}`);
-    console.log(`RETRIES: ${state.context.retries}`);
+const fetchMachineBase: any = {
+  id: 'fetch',
+  initial: 'idle',
+  context: {
+    retries: 0,
+  },
+  states: {
+    idle: {
+      on: {
+        FETCH: 'loading',
+      },
+    },
+    loading: {
+      invoke: {
+        src: "fetchSomething",
+        onDone: 'success',
+        onError: 'failure',
+      },
+    },
+    success: {
+      type: 'final',
+      entry: "logCurrentState",
+    },
+    failure: {
+      on: {
+        RETRY: [{
+          target: "warning",
+          cond: "retryWarningLimitReached"
+        },{
+          target: 'loading',
+          cond: "retryLimitreached"
+        },{
+          target: "error"
+        }]
+      },
+      entry: ["logCurrentState","incrementRetries"],
+    },
+    warning:{
+      always: "loading",
+      entry: "showWarning"
+    },
+    error:{
+      type: "final",
+      entry: "showError"
+    }
+  },
+};
+
+const fetchMachineConfig = {
+    actions: {
+      incrementRetries: assign({
+        retries: (context: any, event) => context.retries + 1,
+      }),
+      logCurrentState: (context, event) => {
+        // console.log(`${context.} entered`);
+        console.log(context, event)
+      },
+      showError: ()=>{
+        //alert("arrggh!")
+      },
+      showWarning: ()=>{
+        //alert("carefull!!!")
+      }
+    },
+    guards: {
+      retryLimitreached:(context) => { 
+        return context.retries < 10
+      },
+      retryWarningLimitReached: (context) => { 
+        return context.retries < 10 && context.retries >= 5
+      },
+    },
+    services: {
+      fetchSomething: () => {
+        return Promise.reject();
+        // return Promise.resolve({});
+      }
+    }
+  };
+
+const fetchMachine = createMachine<any, any>(fetchMachineBase);
+const service = interpret(fetchMachine.withConfig(fetchMachineConfig)).onTransition((state) => {
+  console.log(`CURRENT STATE: ${state.value}`);
+  console.log(`RETRIES: ${state.context.retries}`);
 });
 
 service.start();
 
-service.send("FETCH");
+service.send('FETCH');
 setTimeout(() => {
-    service.send("RETRY");
+  service.send('RETRY');
 }, 0);
 
-btn.addEventListener("click", () => {
-    console.log("clicked");
-    service.send("RETRY");
+btn?.addEventListener('click', () => {
+  console.log('clicked');
+  service.send('RETRY');
 });
